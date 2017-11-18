4005faffce6da8a4a5809ef6b5c4e78efb001113
Edgar Rangel <jakoujan@gmail.com>
2017-11-18 15:58:21 -0600

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PolymerModule } from '@codebakery/origami';
+import { IronElementsModule, PaperElementsModule } from '@codebakery/origami/lib/collections';
 
 import { AppComponent } from './app.component';
 
@@ -9,8 +11,15 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, // Required to connect elements to Angular forms
+    PolymerModule.forRoot(), // Only import .forRoot() once and at the highest level
+ 
+    // Optional modules to help reduce markup complexity
+    IronElementsModule,
+    PaperElementsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
