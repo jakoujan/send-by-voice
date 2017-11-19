8fc4c2fe8b06089e81cae3f02541e199bf8e3988
Edgar Rangel <jakoujan@gmail.com>
2017-11-18 20:20:40 -0600

    Formulario completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Account } from '../app/entity/account';
+import { MockFirebaseService } from "./service/mock-firebase.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   counter: number;
-  ra: Account;
-  aa: Account;
+  wa: Account;
+  da: Account;
+  was: Array<Account>;
+  das: Array<Account>;
+  amount: number;
 
-  constructor() {
+  constructor(private mockService: MockFirebaseService) {
     this.counter = 1;
-    this.ra = new Account();
-    this.aa = new Account();
+    this.wa = new Account(null, null, null);
+    this.da = new Account(null, null, null);
+    this.amount = 0.00;
   }
+
+  ngOnInit(): void {
+    this.was = this.mockService.withdrawalAccouts();
+    this.das = this.mockService.depositAccouts();
+  }
+
 
   onSubmit() {
     console.log('Hola');
--- src/app/entity/account.ts
+++ src/app/entity/account.ts
@@ -3,4 +3,10 @@ export class Account {
     account: string;
     description: string;
 
+    constructor(id: number, account: string, description: string) {
+        this.id = id;
+        this.account = account;
+        this.description = description;
+    }
+
 }
